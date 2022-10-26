c2e86e95b91a3a1623d927a0ac9c2abd75d9825c
zizo-bond <108358567+zizo-bond@users.noreply.github.com>
2022-10-27 00:36:07 +0200

    Update images.ts

--- source/routes/api/images.ts
+++ source/routes/api/images.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-import path from 'path';
+import path from './../../file';
+import fs from 'fs';
 import { resizeImage, resizedImagePath } from '../../utils/imageTransforms';
 import { promises as fsPromises } from 'fs';
-import fs from 'fs';
-
+/* define images*/
 const images = express.Router();
 
 images.get('/', async (req: express.Request, res: express.Response) => {
@@ -11,12 +11,14 @@ images.get('/', async (req: express.Request, res: express.Response) => {
     const filename = req.query.filename as unknown as string;
     const height = parseInt(req.query.height as unknown as string);
     const width = parseInt(req.query.width as unknown as string);
-    const outputImgPath: string = resizedImagePath(filename, height, width);
-    if (!fs.existsSync(outputImgPath)) {
+    const outputPath: string = resizedImagePath(filename, height, width);
+    
+      /* Check if requested file is available*/
+    if (!fs.existsSync(outputPath)) {
       const resizedImage = await resizeImage(filename, height, width);
-      await fsPromises.writeFile(outputImgPath, resizedImage);
+      await fsPromises.writeFile(outputPath, resizedImage);
     }
-    res.sendFile(path.resolve(outputImgPath));
+    res.sendFile(path.resolve(outputPath));
   } catch (err) {
     res.render('errors', { message: err.message });
   }
